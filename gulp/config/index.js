a3a5c6d9e54a056f7bf34927f819412b75f02060
Marcus Nielsen <marcus.nielsen82@gmail.com>
2014-12-16 20:31:07 +0100

    Added firebase rules file.

--- gulp/config/index.js
+++ gulp/config/index.js
@@ -30,7 +30,7 @@ var config = {
     sourcemaps: './maps'
   },
   copy: {
-    source: ['./.gitignore', 'gulpfile.js', 'package.json', 'Procfile', 'firebase.json'],
+    source: ['./.gitignore', 'gulpfile.js', 'package.json', 'Procfile', 'firebase.json', 'firebase-rules.json'],
     dest: './dist'
   },
   lint: {
